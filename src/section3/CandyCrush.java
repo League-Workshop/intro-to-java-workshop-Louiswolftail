7d7272f6c41c2407151ca0fc66ba885fbe6608b8
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-15 13:56:56 -0700

    Day 3

--- src/section3/CandyCrush.java
+++ src/section3/CandyCrush.java
@@ -6,13 +6,13 @@ public class CandyCrush {
 
 	public static void main(String[] args) {
 		// 1. Using a pop-up, ask the user who they don't like
-
+		String name = JOptionPane.showInputDialog("who they don't like?");
 		// 2. Change the next line so that the pop-up will tell them they have a crush on that person
-		JOptionPane.showMessageDialog(null, "You totally have a crush on Java!");
+		JOptionPane.showMessageDialog(null, "You have crush on "+name);
 		// 3. Ask the user for the name of their best friend
-
+String Friend = JOptionPane.showInputDialog("name of their best friend");
 		// 4. Tell them their best friend is as sweet as candy
-
+JOptionPane.showInputDialog(Friend+" is sweet as candy");
 	} 
 }
 
--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,13 +9,14 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+		speak("spell mandlebrot");
 		// 2. Catch the user's answer in a String
-
+String manglebrot = JOptionPane.showInputDialog("enter the word");
 		// 3. If the user spelled the word correctly, speak "correct"
-
+if(manglebrot.equals("spell mandlebrot")){
+}
 		// 4. Otherwise say "wrong"
-
+if()
 		// 5. repeat the process for other words
 		
 	}
